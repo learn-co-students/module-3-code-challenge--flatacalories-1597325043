a8de2f7b2f2d6e92615c9938cf53f4e3793a7ba2
415CA <chrisallen415@gmail.com>
2020-08-13 11:14:00 -0400

    change name completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const name = document.querySelector("#name");
-let calories = document.querySelector("#calories");
+const calories = document.querySelector("#calories");
 const caloriesForm = document.querySelector("#calories-form");
 const resetButton = document.querySelector("#reset-btn");
+const detailedInfo = document.querySelector('#detailed-info');
 
 const characterBar = document.querySelector("#character-bar"); // show characters names here
 const image = document.querySelector("#image"); // display character image here
@@ -20,7 +21,7 @@ fetch(BASE_URL)
 
 const characterHTML = (character) => {
   const API_PATH = `/${character.id}`;
-  let calories = parseInt(character.calories);
+  const calories = parseInt(character.calories);
 
   const characterSpan = document.createElement("span");
   characterBar.append(characterSpan);
@@ -28,13 +29,14 @@ const characterHTML = (character) => {
 
   characterSpan.addEventListener("click", (event) => {
     displayCharacter(character);
+    changeName(character);
 
     caloriesForm.addEventListener("submit", (event) => {
       event.preventDefault();
       const formID = character.id;
 
       const addedCalories = event.target.calories.value;
-      const newCalories = parseInt(calories) + parseInt(addedCalories);
+      const newCalories = parseInt(calories, 10) + parseInt(addedCalories, 10);
 
       fetch(`${BASE_URL}${API_PATH}`, {
         method: "PATCH",
@@ -73,4 +75,38 @@ const displayCharacter = (character) => {
   name.innerText = character.name;
   image.src = character.image;
   calories.innerText = character.calories;
+};
+
+const changeName = (character) => {
+  const API_PATH = `/${character.id}`;
+  
+  nameForm = document.createElement("form");
+  nameInput = document.createElement("input");
+  nameSubmit = document.createElement("button");
+  nameInput.id = 'name'
+  nameSubmit.value = "Edit Name";
+  nameInput.placeholder = 'Edit Name';
+
+  nameForm.append(nameInput, nameSubmit);
+
+  detailedInfo.append(nameForm);
+
+  nameForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    newName = event.target.name.value;
+
+    fetch(`${BASE_URL}${API_PATH}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "Application/json",
+      },
+      body: JSON.stringify({
+        name: newName,
+      }),
+    })
+      .then((response) => response.json())
+      .then((updatedCharacter) => {
+        displayCharacter(updatedCharacter);
+      });
+  });
 };
