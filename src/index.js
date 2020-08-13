521ad4ac19fe74339c1a1941ccae54bbc1741390
Fraciscv <Francisco.Andaur@icloud.com>
2020-08-13 11:37:34 -0400

    done with 2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ const characterDiv = document.querySelector('#character-bar')
 const infoDiv = document.querySelector('.characterInfo')
 const form = document.querySelector('#calories-form')
 
-
-
-console.log(form)
-
 fetch(`http://localhost:3000/characters`)
     .then(r => r.json())
     .then((parsedCharacters) => parsedCharacters.forEach(slapItOnTheDom))
@@ -15,6 +11,7 @@ function slapItOnTheDom(character) {
     const characterSpan = document.createElement('span')
     characterSpan.innerText = character.name
     characterDiv.append(characterSpan)
+    //event listener for click event renders characters to DOM
     characterSpan.addEventListener('click', (e) => {
         infoDiv.innerHTML = `
             <div id="detailed-info">
@@ -22,22 +19,21 @@ function slapItOnTheDom(character) {
                 <img id="image" src="${character.image}"><!-- display character image here -->
                 <h4>Total Calories: <span id="calories">${character.calories}</span> </h4>
                 <form id="calories-form">
-                    <input type="hidden" value="${character.id}" id="characterId"> <!-- Assign character id as a value here -->
+                    <input type="hidden" value="Character's id" id="${character.id}"> <!-- Assign character id as a value here -->
                     <input type="text" placeholder="Enter Calories">
                     <input type="submit" value="Add Calories">
                 </form>
                 <button id="reset-btn">Reset Calories</button>
             </div>
-            `
-                
-    })//end eventListener NOT 100% sure if character.id is in the right place
-
-
+            `          
+    })//end eventListener //updates DOM
 }//end slapItOnTheDom
+
+//TODO: event listener for form
 form.addEventListener('submit', (evt) => {
-    evt.preventDefault()
+    evt.defaultPrevented()
     console.log(evt)
-    const input = ""
+    const input = evt.targer
 
     fetch(`http://localhost:3000/characters/${character.id}`,{
         method: 'PATCH',
@@ -50,18 +46,6 @@ form.addEventListener('submit', (evt) => {
     })
     .then(r => r.json())
     .then(updatedCharacter => {
-        infoDiv.innerHTML = `
-            <div id="detailed-info">
-                <p id="name">${updatedCharacter.name}</p>
-                <img id="image" src="${updatedCharacter.image}"><!-- display character image here -->
-                <h4>Total Calories: <span id="calories">${updatedCharacter.calories}</span> </h4>
-                <form id="calories-form">
-                    <input type="hidden" value="${updatedCharacter.id}" id="characterId"> <!-- Assign character id as a value here -->
-                    <input type="text" placeholder="Enter Calories">
-                    <input type="submit" value="Add Calories">
-                </form>
-                <button id="reset-btn">Reset Calories</button>
-            </div>
-            `
+        
     })
 })
