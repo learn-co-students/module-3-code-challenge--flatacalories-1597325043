26771fa159ed134e633a6758ca072c87837da1fa
Fraciscv <Francisco.Andaur@icloud.com>
2020-08-13 11:06:40 -0400

    30 min warning i think this is all i can do for now

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const characterDiv = document.querySelector('#character-bar')
 const infoDiv = document.querySelector('.characterInfo')
+const form = document.querySelector('#calories-form')
 
 
-console.log(characterDiv)
-console.log(infoDiv)
+
+console.log(form)
 
 fetch(`http://localhost:3000/characters`)
     .then(r => r.json())
@@ -27,8 +28,40 @@ function slapItOnTheDom(character) {
                 </form>
                 <button id="reset-btn">Reset Calories</button>
             </div>
-        `
-    })//end eventListener
+            `
+                
+    })//end eventListener NOT 100% sure if character.id is in the right place
 
-    
+
 }//end slapItOnTheDom
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    console.log(evt)
+    const input = ""
+
+    fetch(`http://localhost:3000/characters/${character.id}`,{
+        method: 'PATCH',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            calories: input
+        })
+    })
+    .then(r => r.json())
+    .then(updatedCharacter => {
+        infoDiv.innerHTML = `
+            <div id="detailed-info">
+                <p id="name">${updatedCharacter.name}</p>
+                <img id="image" src="${updatedCharacter.image}"><!-- display character image here -->
+                <h4>Total Calories: <span id="calories">${updatedCharacter.calories}</span> </h4>
+                <form id="calories-form">
+                    <input type="hidden" value="${updatedCharacter.id}" id="characterId"> <!-- Assign character id as a value here -->
+                    <input type="text" placeholder="Enter Calories">
+                    <input type="submit" value="Add Calories">
+                </form>
+                <button id="reset-btn">Reset Calories</button>
+            </div>
+            `
+    })
+})
