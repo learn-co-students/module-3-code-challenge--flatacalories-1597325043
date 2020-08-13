8c894b04b06cfae99be0054903af66b821b706ac
Rouan Thompson <rouan.thompson@gmail.com>
2020-08-13 11:37:26 -0400

    did not know know to get valuee from form

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
 //     On page load, **request** data from the server to get all of the characters objects. 
 //     When you have this information, 
 //     you'll need to add a `span` tag with the character's name to the character bar.
+// 2. Select a character from the character bar 
+//      and see character's info inside `#detailed-info` div. 
 
 //Stable elements
 const toonsUrl = "http://localhost:3000/characters"
@@ -14,8 +16,9 @@ const infoDiv = document.querySelector("#detailed-info")
 let divName = document.querySelector("#name") 
 let divImage = document.querySelector("#image")
 let divSpan = document.querySelector("#calories")
-
-
+let form = document.querySelector("#calories-form")
+let formToonID = document.querySelector("#characterId")
+//
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
@@ -40,6 +43,7 @@ const renderToons = (toon) => {
         divName.innerText = toon.name
         divImage.src = toon.image
         divSpan.innerText = toon.calories
+        formToonID.value = toon.id
       })
 
     toonSpan.innerText = toon.name
@@ -49,28 +53,22 @@ const renderToons = (toon) => {
 }
 
 
-// 2. Select a character from the character bar 
-//      and see character's info inside `#detailed-info` div. 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // 3. Clicks on "Add Calories" button to add calories to a Character.
+
 //      Persist calories value to the server and update the DOM.
+
+
+form.addEventListener("submit", (form) => {
+    form.preventDefault()
+    fetch(toonsUrl + '/' + formToonID.value, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            calories: form.target.input.value
+            //form.elements[1].placeholder.target.value
+        })
+    })  
+})
