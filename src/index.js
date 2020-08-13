83653f0f4f582be7bc9bc9248b0d93a600c143cf
AlveeM <alvee.akand@outlook.com>
2020-08-13 10:51:04 -0400

    advanced deliverables done

--- src/index.js
+++ src/index.js
@@ -5,10 +5,142 @@ const characterImg = document.getElementById('image');
 const characterCaloriesSpan = document.getElementById('calories');
 const caloriesForm = document.getElementById('calories-form');
 const caloriesResetButton = document.getElementById('reset-btn');
+const updateNameDiv = document.getElementById('update-name');
+const updateNameB = document.getElementById('update-name-btn');
+const addCharacterDiv = document.getElementById('add-character');
+const addCharacterB = document.getElementById('add-character-btn');
 
 fetchAndDisplayCharacterNames();
 caloriesForm.addEventListener('submit', patchCalories);
 caloriesResetButton.addEventListener('click', resetCalories);
+updateNameB.addEventListener('click', toggleUpdateNameForm);
+addCharacterB.addEventListener('click', toggleAddCharacterForm);
+
+function toggleAddCharacterForm(e) {
+  const addCharacterForm = document.getElementById('add-character-form');
+  const hasForm = addCharacterForm !== null
+
+  if (hasForm) {
+    addCharacterForm.remove()
+  } else {
+    const newForm = createNewcharacterForm();
+    addCharacterDiv.append(newForm);
+  }
+}
+
+function createNewcharacterForm() {
+  const form = document.createElement('form');
+    form.id = "add-character-form";
+    form.dataset['charId'] = caloriesForm['characterId'].value;
+    form.addEventListener('submit', postCharacter);
+
+  const inputName = document.createElement('input');
+    inputName.type = "text";
+    inputName.id = "new-character-name";
+    inputName.placeholder = "enter a name";
+    inputName.style.marginRight = "5px";
+
+  const inputImg = document.createElement('input');
+    inputImg.type = "text";
+    inputImg.id = "new-character-image"
+    inputImg.placeholder = "enter an image url"
+    inputImg.style.marginRight = "5px";
+
+  const inputSubmit = document.createElement('input');
+    inputSubmit.type = "submit";
+    inputSubmit.value ="Add";
+
+  form.append(inputName, inputImg, inputSubmit);
+
+  return form;
+}
+
+function postCharacter(e) {
+  e.preventDefault();
+  form = e.target;
+
+  const name = form['new-character-name'].value;
+  const image = form['new-character-image'].value;
+  const calories = 0;
+
+  const postConfig = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      image: image,
+      calories: calories
+    })
+  }
+
+  fetch('http://localhost:3000/characters', postConfig)
+    .then(res => res.json())
+    .then(character => {
+      toggleAddCharacterForm();
+      addToCharacterBar(character);
+      displayCharacter(character);
+    })
+
+  e.target.reset();
+}
+
+function toggleUpdateNameForm(e) {
+  const updateNameForm = document.getElementById('update-name-form');
+  const hasForm = updateNameForm !== null 
+
+  if (hasForm) {
+    updateNameForm.remove();
+  } else {
+    const updateForm = createNameUpdateForm();
+    updateNameDiv.append(updateForm);
+  }
+}
+
+function patchName(e) {
+  e.preventDefault();
+
+  const charId = e.target.dataset['charId'];
+  const updateNameInput = document.getElementById('update-name-input');
+  const newName = updateNameInput.value;
+
+  const patchConfig = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: newName
+    })
+  }
+
+  fetch(`http://localhost:3000/characters/${charId}`, patchConfig)
+    .then(res => res.json())
+    .then(character => displayCharacter(character))
+    .then(_ => toggleUpdateNameForm());
+}
+
+function createNameUpdateForm() {
+  const form = document.createElement('form');
+    form.id = "update-name-form";
+    form.dataset['charId'] = caloriesForm['characterId'].value;
+    form.addEventListener('submit', patchName);
+
+  const inputName = document.createElement('input');
+    inputName.type = "text";
+    inputName.id = "update-name-input";
+    inputName.value = characterNameP.textContent;
+    inputName.style.marginRight = "5px";
+
+  const inputSubmit = document.createElement('input');
+    inputSubmit.type = "submit";
+    inputSubmit.value ="Update";
+
+  form.append(inputName, inputSubmit);
+
+  return form;
+}
 
 function patchCalories(e) {
   e.preventDefault();
